d52563606014a33d6cec141b8ad81f08c2f243d2
mii <kurochian2@gmail.com>
2022-12-06 13:01:32 +0000

    add kubernetes manifest, add code block regex

--- src/tts/validator.rs
+++ src/tts/validator.rs
@@ -2,5 +2,7 @@ use regex::Regex;
 
 pub fn remove_url(text: String) -> String {
     let url_regex = Regex::new(r"(http://|https://){1}[\w\.\-/:\#\?=\&;%\~\+]+").unwrap();
-    url_regex.replace_all(&text, " URL ").to_string()
+    let code_regex = Regex::new(r"```(.*)```").unwrap();
+    let text = url_regex.replace_all(&text, " URL ").to_string();
+    code_regex.replace_all(&text, "code").to_string()
 }
