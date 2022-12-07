2a40e9ee1686ede6519c1f5745ecaea2a305982d
mii <kurochian2@gmail.com>
2022-12-07 03:36:43 +0000

    change regex

--- src/tts/validator.rs
+++ src/tts/validator.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 
 pub fn remove_url(text: String) -> String {
     let url_regex = Regex::new(r"(http://|https://){1}[\w\.\-/:\#\?=\&;%\~\+]+").unwrap();
-    let code_regex = Regex::new(r"```([.\n]*)```").unwrap();
+    let code_regex = Regex::new(r"```(.|\n)*```").unwrap();
     let text = url_regex.replace_all(&text, " URL ").to_string();
     code_regex.replace_all(&text, "code").to_string()
 }
